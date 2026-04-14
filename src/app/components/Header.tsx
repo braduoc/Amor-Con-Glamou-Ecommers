e7677246068e892ebd414707d5bcb14a697e8959
braduoc <bra.chacon@duocuc.cl>
2026-04-14 10:44:32 -0400

    elimine logo test

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -18,14 +18,7 @@ export function Header() {
 
         <div className="flex items-center justify-between h-16">
 
-          {/* LOGO */}
-          <Link to="/" className="flex items-center space-x-2">
-            <img
-              src="/images/logo.jpeg"
-              className="w-40 h-auto object-cover"
-              alt="Amor Con Glamour"
-            />
-          </Link>
+          
 
           {/* DESKTOP NAV */}
           <nav className="hidden md:flex items-center space-x-6">
